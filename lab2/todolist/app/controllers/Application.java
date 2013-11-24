20d8f9b72e1aa04890912fbb29bbd73ee7d9fd21
Ana Luiza <analuizamtg@gmail.com>
2013-11-24 17:29:50 -0300

    colocando tasks

--- lab2/todolist/app/controllers/Application.java
+++ lab2/todolist/app/controllers/Application.java
@@ -2,15 +2,29 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
+import play.data.*;
+import models.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok("Hello world");
-    }
-    
-    
+	static Form<Task> taskForm = Form.form(Task.class);
+
+	
+	public static Result index() {
+		return redirect(routes.Application.tasks());
+	}
+
+	public static Result tasks() {
+		return ok(views.html.index.render(Task.all(), taskForm));
+	}
+
+	public static Result newTask() {
+		return TODO;
+	}
+
+	public static Result deleteTask(Long id) {
+		return TODO;
+	}
 
 }
